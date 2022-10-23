b82c765873c535d4285e3627cf82cfdefb6f57a2
Test <test@gmail.com>
2022-10-23 12:28:03 +0300

    fix main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-      int x = 1000;
+      int x = 5000;
       int y = 20;
       int i = x / y;
         System.out.println(x / y);
