e3b5c4ccb6b8b78273b298b7597c4b9e06e21248
Test <test@gmail.com>
2022-10-28 20:04:31 +0300

    final fix code

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int ticketPrice = 5000;
-        int rubForOneMiles = 20;
-        int i = ticketPrice / rubForOneMiles;
-        System.out.println(ticketPrice / rubForOneMiles);
+        int ticketPrice = 5_000;
+
+        int i = ticketPrice / 20;
+
+        System.out.println("Bonus miles: " + i);
+
 
     }
 }
