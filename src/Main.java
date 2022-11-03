35c76bced1bdb3201f3011e4c3564dffc3f8bb40
Evgeniy <varnavskiyev@gmail.com>
2022-11-03 12:24:27 +0300

    fix main.java

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
         int ticketPrice = 5_000;
 
-        int i = ticketPrice / 20;
+        int bonusMiles = ticketPrice / 20;
 
-        System.out.println("Bonus miles: " + i);
+        System.out.println("Bonus miles: " + bonusMiles);
 
 
     }
